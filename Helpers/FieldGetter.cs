ae67358f929bf52468b258688d9cb58fc1f2c7c2
Skarcz <jakuba.kral@gmail.com>
2026-09-05 21:13:10 +0200

    Added a certificate constructor using a new field getter helper

---
+++ Helpers/FieldGetter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Diagnostics;
+
+namespace CertView.Helpers;
+
+internal class FieldGetter
+{
+
+    public static string? GetSubjectField(X509Certificate2 cert, string oid)
+    {
+        return cert.SubjectName
+        .EnumerateRelativeDistinguishedNames()
+        .Where(x => !x.HasMultipleElements)
+        .FirstOrDefault(x => x.GetSingleElementType().Value == oid)
+        ?.GetSingleElementValue();
+    }
+
+    public static X509Extension? GetExtension(X509Certificate2 cert, string oid)
+    {
+        return cert.Extensions.FirstOrDefault(x => x.Oid?.Value == oid);
+    }
+}
--- Models/CertificateInfo.cs
+++ Models/CertificateInfo.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using CertView.Helpers;
 
 namespace CertView.Models;
 
 internal class CertificateInfo
 {
+    #region Certificate Fields  
     public string? GivenName { get; set; }
     public string? SurName { get; set; }
     public string? CountryName { get; set; }
@@ -17,5 +19,20 @@ internal class CertificateInfo
     public string? Issuer { get; set; }
     public DateTime NotBefore { get; set; }
     public DateTime NotAfter { get; set; }
+    #endregion
+    public CertificateInfo() { }
+    public CertificateInfo(X509Certificate2 certificate)
+    {
+        GivenName = FieldGetter.GetSubjectField(certificate, "2.5.4.42");
+        SurName = FieldGetter.GetSubjectField(certificate, "2.5.4.4");
+        CountryName = FieldGetter.GetSubjectField(certificate, "2.5.4.6");
+        OrganizationName = FieldGetter.GetSubjectField(certificate, "2.5.4.10");
+        OrganizationIdentifier = FieldGetter.GetSubjectField(certificate, "2.5.4.9");
+        OrganizationalUnitNames = FieldGetter.GetSubjectField(certificate, "2.5.4.11")?.Split(',');
+        Title = FieldGetter.GetSubjectField(certificate, "2.5.4.12")?.Split(',');
+        Issuer = certificate.Issuer;
+        NotBefore = certificate.NotBefore;
+        NotAfter = certificate.NotAfter;
+    }
 
 }
