9490f54148f299c430e2cd5334aafb4d444e831d
Skarcz <jakuba.kral@gmail.com>
2026-09-05 21:00:30 +0200

    Switch to single service for all types.

---
+++ Services/CertificateReaderService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using CertView.Models;
+
+namespace CertView.Services;
+ 
+internal class CertificateReaderService : ICertificateReaderService
+{
+    private CertificateInfo? ReadCertificateFromCer(FileInfo fileInfo)
+    {
+        try
+        {
+            throw new NotImplementedException("Reading from .cer files is not implemented yet.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading certificate from .cer file: {ex.Message}");
+            return null;
+        }
+    }
+
+    private CertificateInfo? ReadCertificateFromPfx(FileInfo fileInfo)
+    {
+        try
+        {
+            throw new NotImplementedException("Reading from .pfx files is not implemented yet.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading certificate from .pfx file: {ex.Message}");
+            return null;
+        }
+    }
+    public CertificateInfo? ReadCertificate(FileInfo fileInfo)
+    {
+        switch (fileInfo.Extension.ToLower())
+        {
+            case ".cer":
+                return ReadCertificateFromCer(fileInfo);
+            case ".pfx":
+                return ReadCertificateFromPfx(fileInfo);
+            default:
+                throw new NotSupportedException($"File extension '{fileInfo.Extension}' is not supported.");
+            }
+        }
+
+}
